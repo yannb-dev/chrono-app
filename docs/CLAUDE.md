1d49d7864897e57e32138498da42b207cb04145f
yannb-dev <yannblondeaudev@gmail.com>
2026-08-04 14:18:51 +0200

    clean folder start Module2

---
+++ docs/CLAUDE.md
@@ -0,0 +1,5 @@
+# Consigne de synthèse
+
+
+1. A la commande "Revue pédagogique". Effectues une revue pédagogique des fichiers /backend et /frontend.
+2. A la commande "Edites une synthèse". Prends en compte le fichier roadmap.md dans un premier temps. Puis en fonction de la revue pédagogiques édites le fichier.json.
--- frontend/chronoapp/app/(tabs)/_layout.tsx
+++ frontend/chronoapp/app/(tabs)/_layout.tsx
@@ -1,35 +1,26 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Tabs } from "expo-router";
+import React from "react";
+import { View } from "react-native";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+  // <Tabs> = permet de créer une navbar
+  // Tabs.screen assure le lien entre le btn du navbar et la page via name="accueil" et le nom de fichier accueil.tsx
+  return View;
+  // <Tabs
+  //   screenOptions={{
+  //     tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+  //     headerShown: false,
+  //     tabBarButton: HapticTab,
+  //   }}
+  // >
+  //   <Tabs.Screen
+  //     name="index"
+  //     options={{
+  //       title: "Home",
+  //       tabBarIcon: ({ color }) => (
+  //         <IconSymbol size={14} name="house.fill" color={color} />
+  //       ),
+  //     }}
+  //   />
+  // </Tabs>
 }
