6fa6f50902024c8911bc82fe9e4fd3d6381d521d
yannb-dev <yannblondeaudev@gmail.com>
2026-09-21 10:49:03 +0200

    feat style password NextJs

--- backend/chronoapp/app/component/formResetPassword.tsx
+++ backend/chronoapp/app/component/formResetPassword.tsx
@@ -50,21 +50,29 @@ export default function FormResetPassword({ token }: { token: string }) {
   }
 
   return (
-    <div className="h-[40%] w-300 flex justify-center items-center p-10 rounded-4 bg-gray-300">
+    <div className="h-[40%] w-150 flex justify-center items-center rounded-4 bg-gray-300">
+      <h1 className="text-gray-900 text-sm mt-20 mb-20">
+        Veuillez choisir un nouveau mot de passe
+      </h1>
       <form className="flex flex-col " onSubmit={handleSubmit(onSubmit)}>
         <input
-          className="w-60 border-b border-gray-100 mb-4 outline-none focus:ring-gray-100 focus:ring-1"
+          className="w-100 border-b border-gray-100 mb-4 outline-none focus:ring-gray-100 focus:ring-1"
           {...register("password")}
           placeholder="Nouveau mot de passe"
+          type="password"
         />
         {errors.password && <p>{errors.password.message}</p>}
         <input
-          className="w-60 border-b border-gray-100 mb-20 outline-none focus:ring-gray-100 focus:ring-1"
+          className="w-100 border-b border-gray-100 outline-none focus:ring-gray-100 focus:ring-1"
           {...register("confirmPassword")}
           placeholder="Confirmer nouveau mot de passe"
+          type="password"
         />
         {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
-        <button className="p-4 rounded-sm bg-green-500" type="submit">
+        <button
+          className="w-60 p-4 rounded-sm bg-green-500 mt-16"
+          type="submit"
+        >
           Valider
         </button>
       </form>
--- backend/chronoapp/app/page.tsx
+++ backend/chronoapp/app/page.tsx
@@ -16,11 +16,11 @@ export default async function passwordReset({
         <Image
           src="/images/android-icon-foreground.png"
           alt="Logo de l'application"
-          width={120}
-          height={120}
+          width={200}
+          height={200}
         />
-        <div className="h-80 w-[50%] flex justify-center items-center bg-gray-900 rounded-sm mt-20">
-          <h1 className="text-gray-300 text-sm">
+        <div className="h-30 w-[50%] flex justify-center items-center bg-gray-400 rounded-sm mt-20">
+          <h1 className="text-gray-900 text-sm">
             Lien invalide, veuiller relancer une demande de réinitialisation de
             mot de passe
           </h1>
@@ -29,16 +29,14 @@ export default async function passwordReset({
     );
 
   return (
-    <div className="h-screen w-full flex justify-center items-center font-mono">
+    <div className="h-screen w-full flex flex-col justify-center items-center font-mono">
       <Image
         src="/images/android-icon-foreground.png"
         alt="Logo de l'application"
-        width={120}
-        height={120}
+        width={200}
+        height={200}
       />
-      <h1 className="text-gray-300 text-sm mt-20">
-        Veuillez choisir un nouveau mot de passe
-      </h1>
+
       <FormResetPassword token={token} />
     </div>
   );
