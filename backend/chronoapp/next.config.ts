799356ccdc8f4501b92dc42f48607ab023cf2c05
YannB-181 <yannblondeaudev@gmail.com>
2026-09-26 15:31:21 +0200

    fix(createMail) protection valeur token (#20)
    * fix(createMail) protection valeur token
    
    * fix(passwordReset)remove useState serverComponent

--- backend/chronoapp/next.config.ts
+++ backend/chronoapp/next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [{ key: "Referrer-Policy", value: "no-referrer" }],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
